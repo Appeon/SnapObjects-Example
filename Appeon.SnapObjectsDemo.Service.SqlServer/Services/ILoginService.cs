385c2937d11cc13cf4e9c3dbf9577dd6bbaee39c
appeon <github@appeon.com>
2020-03-31 21:16:58 +0800

    delete

--- Appeon.SnapObjectsDemo.Service.SqlServer/Services/ILoginService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Appeon.SnapObjectsDemo.Services
-{
-    public interface ILoginService
-    {
-        bool UserIsExist(string userName);
-
-        bool Login(string userName,string password);
-    }
-}
